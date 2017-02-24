89ff6847a30bb1325d88747d40a6ce83e41703ec
kcucchi <karina.cucchi@gmail.com>
2017-02-24 11:07:15 -0800

    do weekly counts from cumulative weekly data

--- data/R/R_scripts/scrap_westnile.R
+++ data/R/R_scripts/scrap_westnile.R
@@ -1,5 +1,7 @@
 
 #' 
+#' execute this script from the working directory data/R/R_scripts
+#' 
 #' This script reads case counts data from westnile.ca.gov
 #' Data are reported either yearly or weekly.
 #' 
@@ -56,7 +58,7 @@ scrapeUrl_westnile <- function(url_name,path_save){
   
   write.table(x = table_final,
               file = path_save,
-              quote = F,sep = ';',row.names = F)
+              quote = F,sep = ',',row.names = F)
   
   
 }
@@ -80,16 +82,17 @@ for(year in 2006:2016){
 #' # download weekly cumulative data #
 
 # loop over all years and all weeks in years
+# download data as they are in the website
 
 for(year in 2006:2016){
   
-  for(week in 53){
+  for(week in 1:53){
     
     url_name <- paste0('http://westnile.ca.gov/case_counts.php?year=',
                        year,'&limit_week=',week,'&option=print')
     
     # this is the path to where the data will be saved
-    dir_path <- paste0('../../raw_data/caseCounts_westnile/weekly/',year)
+    dir_path <- paste0('../../raw_data/caseCounts_westnile/weeklyCumulative/',year)
     # create directory of not existing
     if(!dir.exists(dir_path)){dir.create(dir_path)}
     file_path <- paste0(dir_path,'/',year,'_week',week,'.csv')
@@ -99,9 +102,69 @@ for(year in 2006:2016){
   
 }
 
+# now we can read it all again and separate by weekly additions
 
-
-
+for(year in 2006:2016){
+  
+  # this is the path containing all files in year
+  dir_data <- paste0('../../raw_data/caseCounts_westnile/weeklyCumulative/',year)
+  
+  # this is the directory for saving the data
+  dir_save <- paste0('../../raw_data/caseCounts_westnile/weekly/',year)
+  if(!dir.exists(dir_save)){dir.create(dir_save)}
+  
+  # week 1 is just the same as in cumulative folder
+  file_path <- paste0(dir_data,'/',year,'_week1.csv')
+  df_week1 <- read.csv(file = file_path,sep = ',',
+                       colClasses = 'character')
+  df_week1[df_week1=='-'] <- 0
+  path_save <- paste0('../../raw_data/caseCounts_westnile/weekly/',year,
+                      '/',year,'_week1.csv')
+  write.table(x = df_week1,
+              file = path_save,
+              quote = F,sep = ',',row.names = F)
+  
+  # deal will all other weeks
+  for(week in 2:53){
+    
+    # get counts from previous weeks
+    file_path <- paste0(dir_data,'/',year,'_week',(week-1),'.csv')
+    df_previous <- read.csv(file = file_path,sep = ',',
+                            colClasses = 'character')
+    # replace NAs by zeros
+    df_previous[df_previous=='-'] <- 0
+    # turn the dataframe into a matrix
+    data_previous <- data.matrix(df_previous[,c('Human.Cases','Dead.Birds',
+                                                'Mosquito.Samples','Sentinel.Chickens')])
+    
+    # get counts for current week
+    file_path <- paste0(dir_data,'/',year,'_week',week,'.csv')
+    df_current <- read.csv(file = file_path,sep = ',',
+                           colClasses = 'character')
+    # replace NAs by zeros
+    df_current[df_current=='-'] <- 0
+    # turn the dataframe into a matrix
+    data_current <- data.matrix(df_current[,c('Human.Cases','Dead.Birds',
+                                              'Mosquito.Samples','Sentinel.Chickens')])
+    
+    
+    # compute the difference
+    data_diff <- data_current - data_previous
+    
+    # write in csv file
+    data_save <- data.frame(County=df_current$County)
+    data_save <- cbind(data_save,as.data.frame(data_diff))
+    path_save <- paste0('../../raw_data/caseCounts_westnile/weekly/',year,
+                        '/',year,'_week',week,'.csv')
+    write.table(x = data_save,
+                file = path_save,
+                quote = F,sep = ',',row.names = F)
+    
+    rm(df_previous,data_previous,df_current,data_current,data_diff,data_save)
+    
+  }
+  
+}
 
 
 
